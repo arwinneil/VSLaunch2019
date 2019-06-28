3fa2301fd59011191a0f8470f3db03a80dd5fccb
Arwin Neil Baichoo <arwinneil@gmail.com>
2019-06-28 22:30:35 +0400

    Add project files.

---
+++ MachineLearning/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.ML;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace MachineLearning
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            #region Demo
+
+            //var mlContext = new MLContext();
+
+            //string dataPath = "file.csv";
+
+            //IDataView dataset = mlContext.Data.LoadFromTextFile(dataPath);
+
+            //TrainTestData trainTestSplit = mlContext.Data.TrainTestSplit(dataset);
+
+            //IDataView trainingSet = trainTestSplit.TestSet;
+
+            #endregion
+        }
+    }
+}
